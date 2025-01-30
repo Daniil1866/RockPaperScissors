1127422fcbd71323c5fac0242251891901c7fa5c
Daniil <daniilshahov33@gmail.com>
2025-01-30 19:31:21 +0200

    Fix js button logic

--- javascript.js
+++ javascript.js
@@ -1,29 +1,16 @@
-let humanScore = 0, computerScore = 0; // variables that will store the players score
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener('click', playGame);
+
 let logOutput; // paragraph where the game text will be displayed
+logOutput = document.getElementById('output');
+logOutput.innerHTML = "Welcome to the Rock Paper Scissors game!" + '<br>' + '<br>';
 
-// function which randomly returns rock, paper, or scissors
 function getComputerChoice() {
-  // generate a random number from 1 to 10
   let choice = Math.floor(Math.random() * 10) + 1;
-
-  // if it's less than 4, it's rock
   if (choice < 4) return "rock";
-
-  // if it's less than 7, it's paper
   else if (choice < 7) return "paper";
-
-  // in any other case, it's scissors
   else return "scissors";
 }
-
-// function to prompt user's choice, (if he chose an unexisting option, or is cancelling the choice automatically end the game)
-function getHumanChoice() {
-  let choice = prompt("Make your turn");
-  if (choice == null) return "";
-  return choice.toLocaleLowerCase();
-}
-
-// function designed to play a single round
 
 function playRound(humanChoice, computerChoice) {
   // handle null input
@@ -82,31 +69,39 @@ function playRound(humanChoice, computerChoice) {
   return true; // in case if everything goes fine, just return true
 }
 
-
 function playGame() {
-  document.querySelector("#start-button").classList.add("disabled");
-  document.querySelector('#rock-button').classList.remove("disabled");
-  document.querySelector('#paper-button').classList.remove("disabled");
-  document.querySelector('#scissors-button').classList.remove("disabled");
-
+  let humanScore = 0, computerScore = 0;
   logOutput.innerHTML = "";
-  humanScore = computerScore = 0;
 
-  // display the final score, and the winner
+  const buttonContainer = document.querySelector("#button-container");
+  startButton.classList.add("disabled");
+  buttonContainer.classList.remove("disabled");
+  buttonContainer.addEventListener('click', (e) => {
+    switch (e.target.id) {
+      case "rock-button":
+        console.log("rock");
+        break;
+      case "paper-button":
+        console.log("paper");
+        break;
+      case "scissors-button":
+        console.log("scissors");
+        break;
+    }
+  });
+
+
+
+  // while (humanScore <= 5 || computerScore <= 5) {
+  //   if (!playRound(getHumanChoice(), getComputerChoice())) break;
+  //   buttonContainer.forEach(button =>button.classList.remove("disabled"));
+  //   displayFinalScore(humanScore, computerScore);
+  // }
+}
+
+function displayFinalScore(humanScore, computerScore) {
   logOutput.innerHTML += '<br>' + "Final results: " + '<br>';
   logOutput.innerHTML += "Player: " + humanScore + '<br>';
   logOutput.innerHTML += "Computer: " + computerScore + '<br>';
   logOutput.innerHTML += (humanScore === computerScore) ? "Nobody won" : (humanScore > computerScore) ? "Player wins!" : "Computer wins!"
-
-  // while (humanScore <= 5 || computerScore <= 5) {
-  //   if (!playRound(getHumanChoice(), getComputerChoice())) break;
-  // }
 }
-
-// Add a button for starting the game
-const button = document.querySelector('#start-button');
-button.addEventListener('click', playGame);
-
-// Add a paragraph where the game text appears
-logOutput = document.getElementById('output'); // Select the paragraph
-logOutput.innerHTML = "Welcome to the Rock Paper Scissors game!" + '<br>' + '<br>';
